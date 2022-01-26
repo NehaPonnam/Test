6203e2896be0a5b48cf9f7a4eba79102969f301e
NityaHayan <nitya.hayan@gmail.com>
2022-01-26 16:03:00 +0530

    adding poc code

---
+++ runserver.py
@@ -0,0 +1,15 @@
+"""
+This script runs the Transcript application using a development server.
+"""
+
+from os import environ
+
+from TranscriptService import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5000'))
+    except ValueError:
+        PORT = 5000
+    app.run(HOST, PORT)
